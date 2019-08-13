940c37bb8a5dd896da0da6c01af2215e687bead9
Rachael Rice <Rachnicrice@gmail.com>
2019-08-13 14:18:52 -0700

    Created new file locations for pages and CSS. Got constructor fuctions working, started table.

--- js/app.js
+++ js/app.js
@@ -1,136 +1,74 @@
 'use strict';
 
-var storeHours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
+var storeHours = ['6am ', '7am ', '8am ', '9am ', '10am ', '11am ', '12pm ', '1pm ', '2pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ', '8pm '];
 
-var pikeStore = {
-  name: '1st and Pike',
-  minCust: 23,
-  maxCust: 65,
-  avgCookie: 6.3,
-  cookieSales: [],
-  avgCustomer: function (){
-    var randomNumber = Math.random()*((this.maxCust + 1) - this.minCust) + this.minCust;
-    // var customerNumber = Math.floor(randomNumber);
-    return randomNumber;
-  },
-  simSales: function () {
-    var cookiesPurchased = 0;
-    
-    //15 is the number of hours the store is open
-    for (var i = 0; i < 15; i++) {
-      cookiesPurchased = Math.floor(this.avgCookie * this.avgCustomer());
-      this.cookieSales.push(cookiesPurchased);
-    }
-    return [this.cookieSales];
-  },
+
+function Store (name, minCust, maxCust, avgCookie) {
+  this.storeName = name;
+  this.leastCust = minCust;
+  this.mostCust = maxCust;
+  this.cookiePerCust = avgCookie;
+  this.cookieSales = [];
+  Store.cookieStores.push(this);
+}
+
+Store.cookieStores = [];
+
+Store.prototype.randCust = function () {
+  var randomNumber = Math.random()*((this.mostCust + 1) - this.leastCust) + this.leastCust;
+  return randomNumber;
 };
 
-var seaTac = {
-  name: 'SeaTac Airport',
-  minCust: 3,
-  maxCust: 24,
-  avgCookie: 1.2,
-  cookieSales: [],
-  avgCustomer: function (){
-    var randomNumber = Math.random()*((this.maxCust + 1) - this.minCust) + this.minCust;
-    return randomNumber;
-  },
-  simSales: function () {
-    var cookiesPurchased = 0;
+Store.prototype.simSales = function () {
+  var cookiesPurchased = 0;
     
-    //15 is the number of hours the store is open
-    for (var i = 0; i < 15; i++) {
-      cookiesPurchased = Math.floor(this.avgCookie * this.avgCustomer());
-      this.cookieSales.push(cookiesPurchased);
-    }
-    return [this.cookieSales];
-  },
+  //15 is the number of hours the store is open
+  for (var i = 0; i < 15; i++) {
+    cookiesPurchased = Math.floor(this.cookiePerCust * this.randCust());
+    this.cookieSales.push(cookiesPurchased);
+  }
+  return [this.cookieSales];
 };
 
-var seattleCenter = {
-  name: 'Seattle Center',
-  minCust: 11,
-  maxCust: 38,
-  avgCookie: 3.7,
-  cookieSales: [],
-  avgCustomer: function (){
-    var randomNumber = Math.random()*((this.maxCust + 1) - this.minCust) + this.minCust;
-    return randomNumber;
-  },
-  simSales: function () {
-    var cookiesPurchased = 0;
-    
-    //15 is the number of hours the store is open
-    for (var i = 0; i < 15; i++) {
-      cookiesPurchased = Math.floor(this.avgCookie * this.avgCustomer());
-      this.cookieSales.push(cookiesPurchased);
-    }
-    return [this.cookieSales];
-  },
-};
+new Store('1st and Pike', 23, 65, 6.3);
+new Store('SeaTac Airport', 3, 24, 1.2);
+new Store('Seattle Center', 11, 38, 3.7);
+new Store('Capitol Hill', 20, 38, 2.3);
+new Store('Alki', 2, 16, 4.6);
 
-var capitolHill = {
-  name: 'Capitol Hill',
-  minCust: 20,
-  maxCust: 38,
-  avgCookie: 2.3,
-  cookieSales: [],
-  avgCustomer: function (){
-    var randomNumber = Math.random()*((this.maxCust + 1) - this.minCust) + this.minCust;
-    return randomNumber;
-  },
-  simSales: function () {
-    var cookiesPurchased = 0;
-    
-    //15 is the number of hours the store is open
-    for (var i = 0; i < 15; i++) {
-      cookiesPurchased = Math.floor(this.avgCookie * this.avgCustomer());
-      this.cookieSales.push(cookiesPurchased);
-    }
-    return [this.cookieSales];
-  },
-};
+var elementStoreSales = document.getElementById('table-head');
 
-var alki = {
-  name: 'Alki',
-  minCust:2,
-  maxCust:16,
-  avgCookie: 4.6,
-  cookieSales: [],
-  avgCustomer: function () {
-    var randomNumber = Math.random()*((this.maxCust + 1) - this.minCust) + this.minCust;
-    return randomNumber;
-  },
-  simSales: function () {
-    var cookiesPurchased = 0;
-    
-    //15 is the number of hours the store is open
-    for (var i = 0; i < 15; i++) {
-      cookiesPurchased = Math.floor(this.avgCookie * this.avgCustomer());
-      this.cookieSales.push(cookiesPurchased);
-    }
-    return [this.cookieSales];
-  },
-};
+var head = document.getElementById('table-head');
+var body = document.getElementById('table-body');
+var foot = document.getElementById('table-foot');
+head.appendChild(document.createElement('th'));
 
-var cookieStoreNames = [pikeStore.name, seaTac.name, seattleCenter.name, capitolHill.name, alki.name];
+//Creating table rows and giving them IDs
+for (var i = 0; i < Store.cookieStores.length; i++){
+  var newStoreLog = document.createElement('tr');
+  newStoreLog.textContent = Store.cookieStores[i].storeName;
+  body.appendChild(newStoreLog);
+  Store.cookieStores[i].randCust();
+  Store.cookieStores[i].simSales();
+  newStoreLog.id = Store.cookieStores[i].storeName;
+}
+//Creating table columns by time
+for (var x = 0; x < storeHours.length; x++) {
+  var whatHour = document.createElement('th');
+  whatHour.textContent = storeHours[x];
+  head.appendChild(whatHour);
+}
+//Filling in with data from each class to corresponding row
+for (var k = 0; k < Store.cookieStores.length; k++){
+  var storeRow = document.getElementById(Store.cookieStores[k].storeName);
 
-var storeObjects = [pikeStore, seaTac, seattleCenter, capitolHill, alki];
-
-var elementStoreSales = document.getElementById('sales');
-
-for (var i = 0; i < cookieStoreNames.length; i++){
-  var newStoreLog = document.createElement('h4');
-  newStoreLog.textContent = cookieStoreNames[i];
-  elementStoreSales.appendChild(newStoreLog);
-  storeObjects[i].simSales();
-  newStoreLog.id = cookieStoreNames[i];
-
-  for (var j = 0; j < storeHours.length; j++) {
-    var whatHour = document.createElement('li');
-    whatHour.textContent = storeHours[j] + storeObjects[i].cookieSales[j] + ' cookies';
-    elementStoreSales.appendChild(whatHour);
-
-    console.log(whatHour);
+  for (var j = 0; j < storeHours.length; j++){
+    var newCookieEst = document.createElement('td');
+    newCookieEst.textContent = Store.cookieStores[k].cookieSales[j];
+    storeRow.appendChild(newCookieEst);
   }
 }
+
+var totals = document.createElement('tr');
+totals.textContent = 'Total';
+foot.appendChild(totals);
